1499c246cbaece384fcb59910007eed53705c99b
samshabad Bhanuprathap goud <samshabad.goud@m2pfintech.com>
2024-06-29 18:13:51 +0530

    Product Service into git 1

---
+++ Prodouct-Service/src/main/java/com/shopee/product_service/ProdouctServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopee.product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdouctServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdouctServiceApplication.class, args);
+	}
+
+}
---
+++ Prodouct-Service/src/test/java/com/shopee/product_service/ProdouctServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopee.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdouctServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
